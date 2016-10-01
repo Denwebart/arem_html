4e8de6827125bdc3698f1fca9eb6591c2526cd06
denwebart <denwebart@github.com>
2016-10-01 20:36:20 +0300

    HeaderWidget (dropdowns).

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,13 @@
 $(function() {
 
 	// Custom JS
+    $(document).on('click', '.dropdown', function (e) {
+        if($(this).is('.active')) {
+            $(this).removeClass('active');
+        } else {
+            $('.dropdown').removeClass('active');
+            $(this).addClass('active');
+        }
+    });
 
 });
