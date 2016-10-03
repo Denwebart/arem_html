9ae235b8c2c3337ec008bd91991117c507a9892d
denwebart <denwebart@github.com>
2016-10-03 21:37:13 +0300

    Меню для мобильных устройств. Адаптивность header и footer.

--- app/js/common.js
+++ app/js/common.js
@@ -10,4 +10,20 @@ $(function() {
         }
     });
 
+    // Slideout.js Menu
+    var slideout = new Slideout({
+        'panel': document.getElementById('panel'),
+        'menu': document.getElementById('menu'),
+        'padding': 256,
+        'tolerance': 70,
+        'side': 'right'
+    });
+    // Slideout.js Menu - Toggle button
+    document.querySelector('.toggle-button').addEventListener('click', function() {
+        slideout.toggle();
+    });
+    document.querySelector('.menu-close-button').addEventListener('click', function() {
+        slideout.close();
+    });
+
 });
